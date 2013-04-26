8e42910b4dcfe599b436b1c0f7ff341a729de966
andrewjpage <andrewjpage@gmail.com>
2013-04-26 10:30:06 +0100

    working parser plus tests

---
+++ src/parse_fasta.h
@@ -0,0 +1,28 @@
+/*
+ *  Wellcome Trust Sanger Institute
+ *  Copyright (C) 2013  Wellcome Trust Sanger Institute
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef _PARSE_FASTA_H_
+#define _PARSE_FASTA_H_
+
+#include "kseq.h"
+
+void search_for_query(char filename[], char ** search_queries, int number_of_queries);
+int does_string_contain_query(char * input_string, char * input_query);
+
+#endif
---
+++ tests/check_parse_fasta.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <check.h>
+#include "check_parse_fasta.h"
+
+START_TEST (check_does_string_contain_query)
+{
+	char *input_string = ">abc efg";
+	char *input_query  = "abc";
+	char *non_matching_input_query  = "zzz";
+	
+	fail_unless(does_string_contain_query(input_string,input_query) == 1);
+	fail_unless(does_string_contain_query(input_string,non_matching_input_query) == 0);
+	
+}
+END_TEST
+
+START_TEST (check_parsing_fasta_files)
+{
+  char **input_queries;
+	int i;
+  input_queries = (char **) malloc((3+1)*sizeof(char *));
+	for(i = 0; i < 3; i++)
+	{
+		input_queries[i] = (char *) malloc((40)*sizeof(char));
+	}
+	input_queries[0] = "non_matching_query";
+  input_queries[1] = "matching_query";
+  input_queries[2] = "another_matching";
+
+	search_for_query("../tests/data/input_fasta_file", input_queries, 3);
+	fail_unless(1==1);
+}
+END_TEST
+
+
+Suite * parse_fasta_suite(void)
+{
+  Suite *s = suite_create("Parsing a fasta file");
+  TCase *tc_parse_fasta = tcase_create("check_parsing_fasta_files");
+  tcase_add_test(tc_parse_fasta, check_parsing_fasta_files);
+	tcase_add_test(tc_parse_fasta, check_does_string_contain_query);
+  suite_add_tcase(s, tc_parse_fasta);
+  return s;
+}
